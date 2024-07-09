52323ff87d4325cb22a19ef4591dc7e6346415ef
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-07-09 14:46:13 -0300

    finalização da chamada da API,  criação da logo e ajustes nos templates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s514n7sjbo"
+    "revision": "0.20d3qe45tto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
